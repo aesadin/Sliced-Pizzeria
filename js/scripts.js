0eea8ede095584d2a708c2d05ecaba5120f8a14a
Allison Sadin <aesadin@gmail.com>
2020-06-19 10:25:02 -0700

    create html document with form for name and number

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 // Business Logic
 
-function Order(pizza, price) {
+function Order(pizza, price, customerNamer, phoneNumber) {
   this.pizza = [],
-  this.price = price;
+  this.price = price,
+  this.customerName = customerNamer,
+  this.phoneNumber = phoneNumber;
 }
 
 function Pizza(size, toppings, type) {
@@ -11,8 +13,21 @@ function Pizza(size, toppings, type) {
   this.type = type;
 }
 
+let extras = {
+  extra1: "cheese",
+  extra2: "anchovies",
+  extra3: "basil"
+} 
+
+let price = {
+  smPrice: "$14.99",
+  mdPrice: "$18.99",
+  lgPrice: "$24.99"
+}
+
+
 let vegPizza = new Pizza (["sm", "md", "lg"], ["garlic", "mushroom", "spinach"], ["V", "GF", "Reg"]);
 
 let meatPizza = new Pizza (["sm", "md", "lg"], ["sausage", "pepperoni", "ham"], ["V", "GF", "Reg"]);
 
-let classicPizza = new Pizza (["sm", "md", "lg"], ["mozzarella", "basil", "tomato"], ["V", "GF", "Reg"];)
+let classicPizza = new Pizza (["sm", "md", "lg"], ["mozzarella", "basil", "tomato"], ["V", "GF", "Reg"])
