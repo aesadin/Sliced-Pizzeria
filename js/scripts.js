3234815be21d190b44011d00ddd459769ebbfd28
Allison Sadin <aesadin@gmail.com>
2020-06-19 16:50:24 -0700

    changed from let to const on newPizza object to try and console.log it

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,21 @@ function Pizza(size, topping1, topping2, topping3) {
   
 }
 
-function OrderTotal() {
-  this.order = []
+
+
+Pizza.prototype.getTotal = function(size) {
+  for (i = 0; i < this.size.length; i++)
+  if (this.size === "small") {
+    return (14.99 + 12.00)
+  } else if (this.size === "medium") {
+    return (18.99 + 12.00)
+  } else {
+    return (22.99 + 12.00)
+  }
+  
 }
 
-//Pizza.prototype.
+
 
 // UI Logic
 
@@ -23,16 +33,19 @@ $(document).ready(function() {
     const customerName = $("input#customerName").val();
     const phoneNumber = $("input#phoneNumber").val();
 
-    $("h5").html(customerName);
-    $("h5").append( ", " + phoneNumber);
+    $("p").html(customerName);
+    $("p").append( ", " + phoneNumber);
 
     const inputtedSize = $("input:radio[name=size]:checked").val();
     const inputtedTopping1 = $("input:radio[name=firstTopping]:checked").val();
     const inputtedTopping2 = $("input:radio[name=secondTopping]:checked").val();
     const inputtedTopping3 = $("input:radio[name=thirdTopping]:checked").val();
-    let newPizza = new Pizza(inputtedSize, inputtedTopping1, inputtedTopping2, inputtedTopping3);
+    const newPizza = new Pizza(inputtedSize, inputtedTopping1, inputtedTopping2, inputtedTopping3);
+    $("ul").append("<li>" +inputtedSize+ "</li>" + "<li>" +inputtedTopping1+ "<li>" +inputtedTopping2+ "</li>" + "<li>"+inputtedTopping3+ "</li>")
 
-    console.log(newPizza)
+    
+    console.log(Pizza.newPizza[0])
+    
   })
 })
 
